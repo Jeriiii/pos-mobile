e608a90d2f043c00fcf9b5724a6bf8fddc5dbfed
JKoty <jan.kotalik@email.cz>
2015-06-16 11:03:21 +0200

    Proklikávatelná notifikace.

--- app/src/main/java/pos/android/Activities/Chat/Noticing/GlobalNoticer.java
+++ app/src/main/java/pos/android/Activities/Chat/Noticing/GlobalNoticer.java
@@ -2,12 +2,15 @@ package pos.android.Activities.Chat.Noticing;
 
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
 
 import java.util.List;
 
+import pos.android.Activities.Chat.ChatActivity;
 import pos.android.Activities.Chat.Messages.MessageItem;
 import pos.android.R;
 
@@ -32,6 +35,21 @@ public class GlobalNoticer implements INewMessageNoticable, IUnreadedCountNotica
     private void notifyAboutMessage(int userId, String userName, MessageItem messageItem){
         Notification n;
         NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
+        Intent resultIntent = new Intent(context, ChatActivity.class);
+        // This ensures that navigating backward from the Activity leads out of
+        // your application to the Home screen.
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+        // Adds the back stack for the Intent (but not the Intent itself)
+        stackBuilder.addParentStack(ChatActivity.class);
+        // Adds the Intent that starts the Activity to the top of the stack
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent =
+        stackBuilder.getPendingIntent(
+                0,
+                PendingIntent.FLAG_UPDATE_CURRENT
+        );
+
+        builder.setContentIntent(resultPendingIntent);
         n = builder
                 .setContentTitle(userName)
                 .setContentText(messageItem.messageText)
@@ -42,6 +60,7 @@ public class GlobalNoticer implements INewMessageNoticable, IUnreadedCountNotica
                 (NotificationManager) context.getSystemService(context.NOTIFICATION_SERVICE);
         notificationManager.notify(userId, n);
     }
+
 
     public void setContext(Context context) {
         this.context = context;
